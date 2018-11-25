f709e320be03545b582d3a5d26bd7d480cf2aedf
Maul <mprambadi@gmail.com>
2018-11-25 16:28:15 +0700

    Updates

---
+++ precache-manifest.e3b0f3edbb8dcb904d619873c3d55b99.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "4ebad8e7a9f5566e1598",
+    "url": "/todo-react/static/js/runtime~main.4ebad8e7.js"
+  },
+  {
+    "revision": "efe02b43ceed218839ee",
+    "url": "/todo-react/static/js/main.efe02b43.chunk.js"
+  },
+  {
+    "revision": "0006ce036936072184d5",
+    "url": "/todo-react/static/js/1.0006ce03.chunk.js"
+  },
+  {
+    "revision": "efe02b43ceed218839ee",
+    "url": "/todo-react/static/css/main.936bcaa5.chunk.css"
+  },
+  {
+    "revision": "0006ce036936072184d5",
+    "url": "/todo-react/static/css/1.7a9cb440.chunk.css"
+  },
+  {
+    "revision": "cb404f7ff916e515a8dca3e69bf2de25",
+    "url": "/todo-react/index.html"
+  }
+];
